c6ac8446c51b49363d223d7d014a2cfcc2e759ee
lonewolf2799 <46711787+lonewolf2799@users.noreply.github.com>
2019-07-28 18:30:29 +0530

    Add files via upload

---
+++ ex30.py
@@ -0,0 +1,14 @@
+dick = {
+        'name' : 'harshit',
+        'age'  :  20,
+        'place': 'Delhi'
+        }
+        
+        
+for i in dick.values():
+ print (i)        
+ 
+dick['brother'] = 'Vishal'
+
+for x in dick:
+ print (x) 
